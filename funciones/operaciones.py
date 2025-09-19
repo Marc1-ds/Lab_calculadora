819b10d3ce10f6538df6f06ffa2d9064aea67bc9
Marc1-ds <davidypao80@gmail.com>
2025-09-18 20:22:02 -0500

    Agrego la función de multiplicar a operaciones.py

--- funciones/operaciones.py
+++ funciones/operaciones.py
@@ -1,2 +1,9 @@
 def sumar(lista):
     return sum(lista)
+
+def multiplicar(lista):
+    """Recibe una lista de números y devuelve el producto total."""
+    resultado = 1
+    for numero in lista:
+        resultado *= numero
+    return 
